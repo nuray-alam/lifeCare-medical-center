3ddccfd259fb921ced8f72e17a973226c643544a
nuray-alam <siddik4x@gmail.com>
2021-10-20 19:53:58 +0600

    header fixed on top

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,22 +6,22 @@ import "./header.css"
 const Header = () => {
     //setting the user info for showing the user name in navbar
     const { user, logOut } = useAuth();
-    
+
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light" expand="lg" sticky="top">
             <Container className="w-75mx-auto">
                 <Link to="/home" className="fw-bolder fs-2 me-5 text-decoration-none">Life<span className="text-danger">Care</span></Link>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto fs-5">
                         <NavLink as={Link} to="/home" activeStyle={{ fontWeight: "bold", color: "#91BFFF" }} className="text-decoration-none header-link me-3">Home</NavLink>
-                        <NavLink as={Link} to="/consultancy" activeStyle={{ fontWeight: "bold", color: "#91BFFF"}} className="text-decoration-none header-link me-3">Online Consultancy</NavLink>
+                        <NavLink as={Link} to="/consultancy" activeStyle={{ fontWeight: "bold", color: "#91BFFF" }} className="text-decoration-none header-link me-3">Online Consultancy</NavLink>
                         <NavLink as={Link} to="/appointment" activeStyle={{ fontWeight: "bold", color: "#91BFFF" }} className="text-decoration-none header-link me-3">Appointment</NavLink>
                         <NavLink as={Link} to="/about" activeStyle={{ fontWeight: "bold", color: "#91BFFF" }} className="text-decoration-none header-link me-3">About</NavLink>
 
                     </Nav>
                     {/* conditional rendering user name */}
-                    {   
+                    {
                         user.email && <span className="user-name me-2 fw-bold" style={{ color: "#7AA0CE" }}>{user.displayName}</span>
                     }
                     {/* conditional rendering for login and logout button */}
