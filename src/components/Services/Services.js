e90b6d54a634b32d1014ace3cd2e86739511b890
nuray-alam <siddik4x@gmail.com>
2021-10-20 13:38:10 +0600

    minor changes

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -8,7 +8,7 @@ const Services = () => {
     return (
         <div className="services-section my-5 w-75 mx-auto">
                 <h2 className="fw-bolder text-center text-primary mb-5">Our Services</h2>
-                <Row xs={1} md={3} lg={3} className="g-5">
+                <Row xs={1} md={2} lg={3} className="g-5">
                     {
                         services.map(service => <Service key={service.id} service={service}></Service>)
                     }
